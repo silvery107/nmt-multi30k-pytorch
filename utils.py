dd1dd4e9c23896bea5c887d1dd063668f78f1469
SilverSoul <1079931505@qq.com>
2021-05-17 00:23:58 +0800

    update

---
+++ utils.py
@@ -0,0 +1,16 @@
+import torch
+import spacy
+from torchtext.data.metrics import bleu_score
+import sys
+
+
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+
+def load_checkpoint(checkpoint, model, optimizer):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
