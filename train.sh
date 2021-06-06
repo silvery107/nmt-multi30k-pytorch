34fc2f1bfd8cf48cbdabb51f426f78f7a7ef566f
SilverSoul <1079931505@qq.com>
2021-06-06 13:36:11 +0800

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-python go_transformer.py --batch 128 --num-enc 3 --num-dec 3 --emb-dim 256 --ffn-dim 512 --head 8 --dropout 0.3 --epoch 40 --lr 0.0001
+python go_transformer.py \
+    --batch 128 --num-enc 3 --num-dec 3 \
+    --emb-dim 256 --ffn-dim 512 --head 8 \
+    --dropout 0.3 --epoch 40 --lr 0.0001
 
